b3fb08aaaf6d64a1184e32bb0d2c8b5d1742aa79
strahsvid <strahsvid@protonmail.com>
2021-05-03 05:45:24 +0300

    New solution

--- src/Solutions.java
+++ src/Solutions.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Solutions {
 
     // A method that formats seconds into days, hours, minutes and seconds
@@ -27,7 +29,52 @@ public class Solutions {
         );
     }
 
-    public static void main(String[] args) {
-        System.out.println(timeFormat(60));
+
+    /*
+        Task
+
+        Find all integers between m and n (m and n integers such as 1 <= m <= n) such that the sum of their squared divisors is itself a square.
+
+        We will return an array of subarrays or of tuples (in C an array of Pair) or a string. The subarrays
+        (or tuples or Pairs) will have two elements: first the number the squared divisors of which is a square and then the sum of the squared divisors.
+    */
+
+
+    public static String listSquared(long m, long n) {
+
+        StringBuilder result = new StringBuilder("[");
+
+        for (long i = m; i <= n; i++) {
+            String a = findDivisors(i);
+
+            assert a != null;
+
+            if (i != n) result.append(a);
+        }
+
+        return    result.length() == 1 ?
+                  result.append(']').toString() :
+                  result.deleteCharAt(result.length() - 1)
+                        .deleteCharAt(result.length() - 1)
+                        .append(']').toString();
+    }
+
+    private static String findDivisors (long target) {
+
+        ArrayList<Long> divisors = new ArrayList<>();
+
+        long sum = 0;
+
+        for (long i = 1; i <= target; i++) {
+            if (target % i == 0) divisors.add(i);
+        }
+
+        for (Long i: divisors) {
+            sum += Math.pow((double) i, 2);
+        }
+
+        return (long) Math.sqrt(sum) * Math.sqrt(sum) == sum ?
+                String.format("[%d, %d], ", target, sum)
+                : null;
     }
 }
